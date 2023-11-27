34f5dc2ca6a192d5021cb380a2cbe8467a7536fd
Fardeen Ehsan <fardeen.es7@gmail.com>
2023-11-27 11:07:03 +0600

    ..

--- lib/manage/fetchers.ts
+++ lib/manage/fetchers.ts
@@ -1,38 +1,27 @@
 import { unstable_cache } from "next/cache";
 import prisma from "@/lib/prisma";
 import { serialize } from "next-mdx-remote/serialize";
-import { replaceExamples, replaceTweets } from "@/lib/remark-plugins";
+import { replaceTweets } from "@/lib/manage/remark-plugins";
 
-export async function getSiteData(domain: string) {
-  const subdomain = domain.endsWith(`.${process.env.NEXT_PUBLIC_APP_URL}`)
-    ? domain.replace(`.${process.env.NEXT_PUBLIC_APP_URL}`, "")
-    : null;
-
-  return await unstable_cache(
-    async () => {
-      return prisma.site.findUnique({
-        where: subdomain ? { subdomain } : { customDomain: domain },
-        include: { user: true },
-      });
-    },
-    [`${domain}-metadata`],
-    {
-      revalidate: 900,
-      tags: [`${domain}-metadata`],
-    },
-  )();
+export async function getCourseData(slug: string) {
+  return await unstable_cache(async () => {
+    return prisma.course.findUnique({
+      where: slug ? { slug } : { slug: slug },
+      include: { user: true },
+    });
+  })();
 }
 
-export async function getPostsForSite(domain: string) {
-  const subdomain = domain.endsWith(`.${process.env.NEXT_PUBLIC_APP_URL}`)
+export async function getLessonsForCourse(domain: string) {
+  const slug = domain.endsWith(`.${process.env.NEXT_PUBLIC_APP_URL}`)
     ? domain.replace(`.${process.env.NEXT_PUBLIC_APP_URL}`, "")
     : null;
 
   return await unstable_cache(
     async () => {
-      return prisma.post.findMany({
+      return prisma.lesson.findMany({
         where: {
-          site: subdomain ? { subdomain } : { customDomain: domain },
+          course: slug ? { slug } : { slug: slug },
           published: true,
         },
         select: {
@@ -50,83 +39,71 @@ export async function getPostsForSite(domain: string) {
         ],
       });
     },
-    [`${domain}-posts`],
+    [`${domain}-lessons`],
     {
       revalidate: 900,
-      tags: [`${domain}-posts`],
+      tags: [`${domain}-lessons`],
     },
   )();
 }
 
-export async function getPostData(domain: string, slug: string) {
-  const subdomain = domain.endsWith(`.${process.env.NEXT_PUBLIC_APP_URL}`)
-    ? domain.replace(`.${process.env.NEXT_PUBLIC_APP_URL}`, "")
-    : null;
+export async function getLessonData(slug: string) {
+  return await unstable_cache(async () => {
+    const data = await prisma.lesson.findFirst({
+      where: {
+        slug: slug,
+        published: true,
+      },
+      include: {
+        course: {
+          include: {
+            user: true,
+          },
+        },
+      },
+    });
 
-  return await unstable_cache(
-    async () => {
-      const data = await prisma.post.findFirst({
+    if (!data) return null;
+
+    const [mdxSource, adjacentLessons] = await Promise.all([
+      getMdxSource(data.content!),
+      prisma.lesson.findMany({
         where: {
-          site: subdomain ? { subdomain } : { customDomain: domain },
-          slug,
+          course: slug ? { slug } : { slug: slug },
           published: true,
-        },
-        include: {
-          site: {
-            include: {
-              user: true,
-            },
+          NOT: {
+            id: data.id,
           },
         },
-      });
+        select: {
+          slug: true,
+          title: true,
+          createdAt: true,
+          description: true,
+          image: true,
+          imageBlurhash: true,
+        },
+      }),
+    ]);
 
-      if (!data) return null;
-
-      const [mdxSource, adjacentPosts] = await Promise.all([
-        getMdxSource(data.content!),
-        prisma.post.findMany({
-          where: {
-            site: subdomain ? { subdomain } : { customDomain: domain },
-            published: true,
-            NOT: {
-              id: data.id,
-            },
-          },
-          select: {
-            slug: true,
-            title: true,
-            createdAt: true,
-            description: true,
-            image: true,
-            imageBlurhash: true,
-          },
-        }),
-      ]);
-
-      return {
-        ...data,
-        mdxSource,
-        adjacentPosts,
-      };
-    },
-    [`${domain}-${slug}`],
-    {
-      revalidate: 900, // 15 minutes
-      tags: [`${domain}-${slug}`],
-    },
-  )();
+    return {
+      ...data,
+      mdxSource,
+      adjacentLessons,
+    };
+  })();
 }
 
-async function getMdxSource(postContents: string) {
+async function getMdxSource(lessonContents: string) {
   // transforms links like <link> to [link](link) as MDX doesn't support <link> syntax
   // https://mdxjs.com/docs/what-is-mdx/#markdown
   const content =
-    postContents?.replaceAll(/<(https?:\/\/\S+)>/g, "[$1]($1)") ?? "";
+    lessonContents?.replaceAll(/<(https?:\/\/\S+)>/g, "[$1]($1)") ?? "";
   // Serialize the content string into MDX
   const mdxSource = await serialize(content, {
-    mdxOptions: {
-      remarkPlugins: [replaceTweets, () => replaceExamples(prisma)],
-    },
+    // mdxOptions: {
+    //   remarkPlugins: [replaceTweets, () => replaceExamples(prisma)],
+    // },
   });
 
   return mdxSource;
