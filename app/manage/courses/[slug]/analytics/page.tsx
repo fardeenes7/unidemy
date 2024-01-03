f569920e79ff275d4512500baf1e761ee3e1f8f0
Fardeen Ehsan <fardeen.es7@gmail.com>
2024-01-03 20:55:59 +0600

    bug fix

--- app/manage/courses/[slug]/analytics/page.tsx
+++ app/manage/courses/[slug]/analytics/page.tsx
@@ -28,7 +28,7 @@ export default async function SiteAnalytics({
       <div className="flex items-center justify-center sm:justify-start">
         <div className="flex flex-col items-center space-x-0 space-y-2 sm:flex-row sm:space-x-4 sm:space-y-0">
           <h1 className="font-display text-xl font-bold dark:text-white sm:text-3xl">
-            Analytics for {data.name}
+            Analytics for {data.title}
           </h1>
           <a
             href={`https://${url}`}
